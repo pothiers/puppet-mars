da68bc7f96e023a0b53768835c1eb6f5b5a60a8c
Steve Pothier <pothiers@gmail.com>
2016-08-17 12:04:11 -0700

    snap

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,7 +18,7 @@ class mars::install (
     provider => git,
     source   => 'https://github.com/pothiers/mars.git',
     #!revision => 'master',
-    revision => '1.4rc2',
+    revision => '1.4rc3',
     notify   =>  Python::Requirements [ '/opt/mars/requirements.txt'],
   }
 
