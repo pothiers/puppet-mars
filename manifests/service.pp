76eb46f01bba3acf2783f030214bb77bb9f1b6a5
Steve Pothier <pothiers@gmail.com>
2017-11-16 13:49:12 -0700

    start server UNLESS already running

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,18 @@
 class mars::service  (
   $djangoserver = hiera('djangoserver', '/opt/mars/start-mars-production.sh'),
   ) {
-  #notice("Loading mars::service.pp")    # output to puppet master
-  #notify{ "Loading mars::service.pp": } # output to puppet client
+  #notice("Loading mars::service.pp")  # output to puppet master
+  notify{ "Loading mars::service.pp; ${djangoserver}": } # output to puppet client
   exec { 'collect status':
     command => "/bin/bash -c 'source /opt/mars/venv/bin/activate; /opt/mars/marssite/manage.py collectstatic'",
     creates => '/opt/mars/marssite/audit/static/audit/screen.css',
-    } ->
+    } 
   exec { 'start mars':
     cwd     => '/opt/mars',
     #command => "/bin/bash -c 'source /opt/mars/venv/bin/activate; /opt/mars/marssite/start-mars.sh'",
     command => "/bin/bash -c ${djangoserver}",
-    creates => '/var/log/mars/mars-detail.log',
-    #unless  => '/usr/bin/pgrep -f "manage.py runserver"',
+    #creates => '/var/log/mars/mars-detail.log',
+    unless  => '/usr/bin/pgrep -f "manage.py runserver"',
     user    => 'devops',
     subscribe => [
       Vcsrepo['/opt/mars'], 
